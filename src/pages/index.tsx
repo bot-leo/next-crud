de91c548539c324dbb521a35da6c091fa24ff6e7
Leonardo Lima <leonardo98lima@outlook.com>
2021-10-28 00:41:46 -0300

    iniciando e estruturando o projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className={`flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 via-yellow-500 to-blue-600`}>
+      <h1 className="text-4xl">Primeiro App com Next</h1>
+    </div>
+  )
+}
